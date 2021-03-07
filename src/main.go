f7ed8cbc1f24ede075f6a19d3755299619476efc
Manuel Lang <manuellang183@gmail.com>
2021-03-07 15:07:49 +0100

    seperated main and operations

--- src/main.go
+++ src/main.go
@@ -2,19 +2,9 @@ package main
 
 import "fmt"
 
-var z int
-
-func add(x int, y int) int {
-	return x + y
-}
-
-func sub(x int, y int) int {
-	return x - y
-}
-
 func main() {
-	sevenPlusFive := add(7, 5)
-	sevenMinusFive := sub(7, 5)
+	sevenPlusFive := Add(7, 5)
+	sevenMinusFive := Sub(7, 5)
 	fmt.Printf("7 + 5 = %d \n", sevenPlusFive)
 	fmt.Printf("7 - 5 = %d \n", sevenMinusFive)
 }
---
+++ src/operations.go
@@ -0,0 +1,11 @@
+package main
+
+var z int
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func Sub(x int, y int) int {
+	return x - y
+}
