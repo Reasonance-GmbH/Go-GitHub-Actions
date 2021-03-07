719fd6629ccac873a438a986e6c8873a07d435b9
Manuel Lang <manuellang183@gmail.com>
2021-03-07 16:39:13 +0100

    comments

--- src/operations.go
+++ src/operations.go
@@ -2,10 +2,12 @@ package main
 
 var z int
 
+// Add adds to integers and returns the value
 func Add(x int, y int) int {
 	return x + y
 }
 
+// Sub subtracts to integers and returns the value
 func Sub(x int, y int) int {
 	return x - y
 }
