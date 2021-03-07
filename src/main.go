19626a588791172c434c9fc4639349eb55c63dc3
Manuel Lang <manuellang183@gmail.com>
2021-03-07 18:05:07 +0100

    Update main.go

--- src/main.go
+++ src/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	operations "manuel-lang/Go-GitHub-Actions-Dependency"
 )
 
 func main() {
-	sevenPlusFive := operations.Add(7, 5)
-	sevenMinusFive := operations.Sub(7, 5)
+	sevenPlusFive := Add(7, 5)
+	sevenMinusFive := Sub(7, 5)
 	fmt.Printf("7 + 5 = %d \n", sevenPlusFive)
 	fmt.Printf("7 - 5 = %d \n", sevenMinusFive)
 }
