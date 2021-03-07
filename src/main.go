35ed98065b00d6f35e8edb390b055f5832cb3325
Manuel Lang <manuellang183@gmail.com>
2021-03-07 15:04:58 +0100

    example main

--- src/main.go
+++ src/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 var z int
 
 func add(x int, y int) int {
@@ -8,4 +10,11 @@ func add(x int, y int) int {
 
 func sub(x int, y int) int {
 	return x - y
+}
+
+func main() {
+	sevenPlusFive := add(7, 5)
+	sevenMinusFive := sub(7, 5)
+	fmt.Printf("7 + 5 = %d \n", sevenPlusFive)
+	fmt.Printf("7 - 5 = %d \n", sevenMinusFive)
 }
