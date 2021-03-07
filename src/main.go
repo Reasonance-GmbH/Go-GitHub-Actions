ec1bdcbb03e9a900aef07198538c22862bde5082
Manuel Lang <manuellang183@gmail.com>
2021-03-07 17:15:04 +0100

    using external package

--- src/main.go
+++ src/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	operations "manuel-lang/Go-GitHub-Actions-Dependency"
+)
 
 func main() {
-	sevenPlusFive := Add(7, 5)
-	sevenMinusFive := Sub(7, 5)
+	sevenPlusFive := operations.Add(7, 5)
+	sevenMinusFive := operations.Sub(7, 5)
 	fmt.Printf("7 + 5 = %d \n", sevenPlusFive)
 	fmt.Printf("7 - 5 = %d \n", sevenMinusFive)
 }
